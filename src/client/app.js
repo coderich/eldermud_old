ffa5fe2b8ab1589f8a0d633e1a11e8b418d95b54
CodeRich <richard.livolsi@gmail.com>
2018-11-24 21:21:18 -0500

    Little better client

--- src/client/app.js
+++ src/client/app.js
@@ -8,7 +8,6 @@ const EldermudIO = IO('http://localhost:3000/realm/eldermud');
 ServerIO.on('request', (event, cb) => {
   cb(new Date().getTime());
 });
-
 // ServerIO.on('connect', () => {});
 // ServerIO.on('reconnect', () => {});
 // ServerIO.on('disconnect', (reason) => {
@@ -17,27 +16,64 @@ ServerIO.on('request', (event, cb) => {
 //   }
 // });
 
+let query = '';
 
-const userAction = () => {
-  ReadInterface.question('> ', (text) => {
-    EldermudIO.emit('input', { type: 'text', payload: { text } });
-    userAction();
-  });
+const clearLine = () => {
+  process.stdout.clearLine();
 };
 
+const write = (text) => {
+  clearLine();
+  process.stdout.cursorTo(0);
+  process.stdout.write(`> ${text}`);
+};
+
+const writeLine = (line) => {
+  clearLine();
+  process.stdout.cursorTo(0);
+  process.stdout.write(`| ${line}\n`);
+  write(query);
+  // console.log(line);
+};
+
+process.stdin.on('keypress', (chunk, key) => {
+  switch (key.name) {
+    case 'return':
+      EldermudIO.emit('input', { type: 'text', payload: { text: query } });
+      query = '';
+      write(query);
+      break;
+    case 'backspace':
+      query = query.slice(0, -1);
+      break;
+    default:
+      if (!key.code) {
+        query += chunk;
+      }
+      break;
+  }
+});
+
+// const userAction = () => {
+//   ReadInterface.question(`> ${query}`, (text) => {
+//     EldermudIO.emit('input', { type: 'text', payload: { text: query } });
+//     query = '';
+//     userAction();
+//   });
+// };
+
 EldermudIO.on('connect', () => {
-  userAction();
 });
 
 EldermudIO.on('output', (event) => {
   switch (event.type.toLowerCase()) {
     case 'info':
-      console.log(event.payload.text);
+      writeLine(event.payload.text);
       break;
     case 'brief':
-      console.log(event.payload.name);
-      console.log(event.payload.description);
-      console.log('Exits: ', event.payload.exits);
+      writeLine(event.payload.name);
+      writeLine(event.payload.description);
+      writeLine(`Exits: ${Object.keys(event.payload.exits).join(', ')}`);
       break;
     default:
       break;
